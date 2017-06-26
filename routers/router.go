2eb4773a9ca75e12ec7ae587435864fc6e718f90
jotavargas <jotavargas89@gmial.com>
2017-06-25 21:30:57 -0500

    #ajustes a nombre de servicio

--- routers/router.go
+++ routers/router.go
@@ -30,7 +30,7 @@ func init() {
 				&controllers.RegistroPresupuestalController{},
 			),
 		),
-		beego.NSNamespace("/Orden_pago_nomina",
+		beego.NSNamespace("/orden_pago_nomina",
 			beego.NSInclude(
 				&controllers.OrdenPagoNominaController{},
 			),
