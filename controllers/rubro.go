662333c00cf7125db259d709674f6405eeda64e9
Miguel Ramirez <miguelramirez93@localhost.localdomain>
2017-07-31 11:18:29 -0500

    Cambios ttemporales prueba proy pac

--- controllers/rubro.go
+++ controllers/rubro.go
@@ -1,12 +1,11 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
-	"strconv"
 
 	"github.com/astaxie/beego"
 	"github.com/udistrital/api_mid_financiera/models"
-	"github.com/udistrital/api_mid_financiera/utilidades"
 )
 
 type RubroController struct {
@@ -16,71 +15,23 @@ type RubroController struct {
 // GenerarPac ...
 // @Title GenerarPac
 // @Description Get PAC By Rubro
-// @Param	vigencia		path 	string	true		"vigencia del pac para los rubros"
-// @Param	finicio		query 	string	true		"fecha inicial del rango del PAC"
-// @Param	ffin		query 	string	true		"fecha final del rango del PAC"
+// @Param	pacData		query 	interface{}	true		"objeto con fechas del rango del PAC"
 // @Success 200 {object} interface{}
 // @Failure 403
-// @router /GenerarPac/:vigencia [get]
+// @router /GenerarPac/ [post]
 func (c *RubroController) GenerarPac() {
-	vigStr := c.Ctx.Input.Param(":vigencia")
-	vigencia, err := strconv.Atoi(vigStr)
-	finicioStr := c.GetString("finicio")
-	ffinStr := c.GetString("ffin")
-	if len(finicioStr) == 0 || len(ffinStr) == 0 {
-
-		c.Data["json"] = models.Alert{Code: "E_XXX", Body: nil, Type: "error"}
-		c.ServeJSON()
-	} else {
-		var idapr string
-		var idfuente string
-		var rubrosHijos []map[string]interface{}
-
-		if err != nil {
-			fmt.Println(err.Error())
-			c.Data["json"] = models.Alert{Code: "E_XXX", Body: err.Error(), Type: "error"}
-			c.ServeJSON()
+	var pacData map[string]interface{} //definicion de la interface que recibe los datos del reporte y proyecciones
+	var reporteData map[string]interface{}
+	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &pacData); err == nil {
+		if err := sendJson("http://"+beego.AppConfig.String("Urlcrud")+":"+beego.AppConfig.String("Portcrud")+"/"+beego.AppConfig.String("Nscrud")+"/rubro/RubroReporte", "POST", &reporteData, &pacData); err == nil {
+			c.Data["json"] = reporteData
 		} else {
-			if err := getJson("http://"+beego.AppConfig.String("Urlcrud")+":"+beego.AppConfig.String("Portcrud")+"/"+beego.AppConfig.String("Nscrud")+"/apropiacion/GetApropiacionesHijo/"+strconv.Itoa(vigencia)+"?tipo=2", &rubrosHijos); err == nil {
-				for _, rubro := range rubrosHijos {
-					var ingresoRubro []map[string]interface{}
-					err = utilidades.FillStruct(rubro["id"], &idapr)
-					if err != nil {
-						fmt.Println(err.Error())
-						c.Data["json"] = models.Alert{Code: "E_XXX", Body: err.Error(), Type: "error"}
-						c.ServeJSON()
-					}
-					if rubro["idfuente"] != nil {
-						err = utilidades.FillStruct(rubro["idfuente"], &idfuente)
-						if err != nil {
-							fmt.Println(err.Error())
-							c.Data["json"] = models.Alert{Code: "E_XXX", Body: err.Error(), Type: "error"}
-							c.ServeJSON()
-						}
-						//fmt.Println("rubro ", idrubro)
-						fmt.Println("fuente ", rubro["idfuente"])
-					} else {
-						idfuente = "0"
-					}
-					if err := getJson("http://"+beego.AppConfig.String("Urlcrud")+":"+beego.AppConfig.String("Portcrud")+"/"+beego.AppConfig.String("Nscrud")+"/rubro/GetRubroIngreso?apropiacion="+idapr+"&fuente="+idfuente+"&finicio="+finicioStr+"&ffin="+ffinStr, &ingresoRubro); err == nil {
-						if ingresoRubro != nil {
-							fmt.Println(ingresoRubro)
-						}
-
-					}
-					idapr = ""
-					idfuente = ""
-
-				}
-
-				c.Data["json"] = rubrosHijos
-			} else {
-				fmt.Println(err.Error())
-				c.Data["json"] = models.Alert{Code: "E_XXX", Body: err.Error(), Type: "error"}
-			}
+			fmt.Println("err 2")
+			c.Data["json"] = models.Alert{Code: "E_0458", Body: err.Error(), Type: "error"}
 		}
-
-		c.ServeJSON()
+	} else {
+		fmt.Println("err 1")
+		c.Data["json"] = models.Alert{Code: "E_0458", Body: err.Error(), Type: "error"}
 	}
-
+	c.ServeJSON()
 }
