c488afdc95a7cfdbafb74c772e51025cf7aee1b5
miguelramirez93 <darckzios@gmail.com>
2017-10-18 11:23:06 -0500

    Correccion estructura rubro

--- models/rubro.go
+++ models/rubro.go
@@ -1,10 +1,10 @@
 package models
 
 type Rubro struct {
-	Id              int64    `pk;orm:"column(id);serial"`
-	Entidad         *Entidad `orm:"column(entidad);rel(fk)"`
-	Codigo          string   `orm:"column(codigo);null"`
-	Vigencia        float64  `orm:"column(vigencia)"`
+	Id              int64   `pk;orm:"column(id);serial"`
+	Entidad         int     `orm:"column(entidad);rel(fk)"`
+	Codigo          string  `orm:"column(codigo);null"`
+	Vigencia        float64 `orm:"column(vigencia)"`
 	Nombre          string
 	Descripcion     string `orm:"column(descripcion);null"`
 	TipoPlan        int16  `orm:"column(tipo_plan);null"`
