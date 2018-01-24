560bf17f2e6cb4dc5432fbd25035caa9701b3eaa
jjvargass <jotavargas89@gmail.com>
2018-01-24 12:37:23 -0500

    #19295# creación de servicio en un 20%

--- controllers/orden_pago.go
+++ controllers/orden_pago.go
@@ -5,7 +5,10 @@ import (
 	// "strconv"
 	// "strings"
 
+	"strconv"
+
 	"github.com/astaxie/beego"
+	"github.com/udistrital/api_financiera/utilidades"
 	"github.com/udistrital/ss_crud_api/models"
 	// "github.com/udistrital/api_mid_financiera/utilidades"
 )
@@ -30,22 +33,59 @@ func (c *OrdenPagoController) URLMapping() {
 // @Failure 403 body is empty
 // @router /GetOrdenPagoByFuenteFinanciamiento [get]
 func (c *OrdenPagoController) GetOrdenPagoByFuenteFinanciamiento() {
-	println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa")
 	fuente := c.GetString("fuente")
 	vigencia := c.GetString("vigencia")
 	unidadEjecutora := c.GetString("unidadEjecutora")
 	if fuente != "" && vigencia != "" && unidadEjecutora != "" {
-		println("AAAAAAAAAAAAAAA")
-		// if mensaje.Type == "success" {
-		// 	c.Data["json"] = Ordenes
-		// } else {
-		// 	c.Ctx.Output.SetStatus(201)
-		// 	c.Data["json"] = mensaje
-		// }
+		var registro_presupuestales []interface{}
+		respuestas := make(map[string]interface{})
+		// search registro_presupuestal
+		if err := getJson("http://"+beego.AppConfig.String("kronosService")+"registro_presupuestal/?query=RegistroPresupuestalDisponibilidadApropiacion.DisponibilidadApropiacion.FuenteFinanciamiento.Id:"+fuente+",Vigencia:"+vigencia+"&limit:-1", &registro_presupuestales); err == nil && registro_presupuestales != nil {
+			println("1111111")
+			var outputOrdenPago []interface{}
+			done := make(chan interface{})
+			defer close(done)
+			resch := utilidades.GenChanInterface(registro_presupuestales...)
+			chlistaOrdenes := utilidades.Digest(done, searchOrdenPagoByRpId, resch, nil)
+			for arrayOrdenPago := range chlistaOrdenes {
+				if dataOrden, e := arrayOrdenPago.([]interface{}); e {
+					for _, ordenPago := range dataOrden {
+						outputOrdenPago = append(outputOrdenPago, ordenPago.(interface{}))
+					}
+				}
+			}
+			if outputOrdenPago != nil {
+				respuestas["OrdenPago"] = outputOrdenPago
+				c.Data["json"] = respuestas
+			} else {
+				c.Data["json"] = nil
+			}
+		} else {
+			c.Data["json"] = nil
+		}
 	} else {
 		c.Ctx.Output.SetStatus(201)
 		c.Data["json"] = models.Alert{Code: "E_0458", Body: "Not enough parameter in GetOrdenPagoByFuenteFinanciamiento", Type: "error"}
 	}
 	c.ServeJSON()
 
+}
+
+func searchOrdenPagoByRpId(registro_resupuestal interface{}, params ...interface{}) (res interface{}) {
+	rowRp, e := registro_resupuestal.(map[string]interface{})
+	if e {
+		var ordenes_pagos []interface{}
+		// if err := getJson("http://"+beego.AppConfig.String("kronosService")+"orden_pago/?query=RegistroPresupuestal.Id:200", &ordenes_pagos); err == nil && ordenes_pagos != nil {
+		// 	return ordenes_pagos
+		// }
+
+		if err := getJson("http://"+beego.AppConfig.String("kronosService")+"orden_pago/?query=RegistroPresupuestal.Id:"+strconv.Itoa(int(rowRp["Id"].(float64))), &ordenes_pagos); err == nil && ordenes_pagos != nil {
+			for _, orden := range ordenes_pagos {
+				row := orden.(map[string]interface{})
+				row["RegistroPresupuestal"] = rowRp
+			}
+			return ordenes_pagos
+		}
+	}
+	return
 }
