7af113494535b2d7b088937dd478ef8aead0317e
Yeison <sistemas@localhost.localdomain>
2018-06-19 17:03:49 -0500

    obtener organizaciones

--- controllers/organizaciones_core_new.go
+++ controllers/organizaciones_core_new.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/astaxie/beego"
 	"github.com/udistrital/utils_oas/request"
+	"github.com/udistrital/utils_oas/optimize"
 )
 
 // OrganizacionesCoreNewController operations for OrganizacionesCoreNew
@@ -70,12 +71,14 @@ func (c *OrganizacionesCoreNewController) GetAll() {
 // @Success 200 {object} models.OrganizacionesCoreNew
 // @Failure 403
 // @router getOrganizacion/
-func (c *OrganizacionesCoreNewController) GetOrganizacion() {
-	beego.Error("entra a get organizacion");
-	var ente []map[string]interface{}
+func (c *OrganizacionesCoreNewController) GetOrganizacion()() {
+
+	var ente  []interface{}
+	var tipoEnte []map[string]interface{}
 	var limit int64 = 10
 	var offset int64
 	var query string
+	var respuesta []map[string]interface{}
 	// limit: 10 (default is 10)
 	if v, err := c.GetInt64("limit"); err == nil {
 		limit = v
@@ -89,14 +92,71 @@ func (c *OrganizacionesCoreNewController) GetOrganizacion() {
 	}
 
 
-	if err := request.GetJson("http://"+"pruebasapi.intranetoas.udistrital.edu.co"+":8083"+"/v1/tipo_ente", &ente); err == nil {
-  			beego.Info("http://pruebasapi.intranetoas.udistrital.edu.co:8083/v1/"+"tipo_ente?limit="+strconv.FormatInt(limit,10)+"&offset="+strconv.FormatInt(offset, 10)+"&query="+query);
-  			beego.Info(ente);
-	}else{
-  			beego.Info("http://"+"pruebasapi.intranetoas.udistrital.edu.co"+":8083"+"/v1/tipo_ente");
-  			beego.Error(err);
-	}
+	if err := request.GetJson(beego.AppConfig.String("coreEnteService")+"tipo_ente?limit="+strconv.FormatInt(limit,10)+"&offset="+strconv.FormatInt(offset, 10)+"&query="+query, &tipoEnte); err == nil {
+				beego.Info(tipoEnte);
+				idEnte:=int(tipoEnte[0]["Id"].(float64));
+				beego.Error(beego.AppConfig.String("coreEnteService")+"ente?limit=-1&query=TipoEnte.Id:"+strconv.Itoa(idEnte))
+				if request.GetJson(beego.AppConfig.String("coreEnteService")+"ente?limit=-1&query=TipoEnte.Id:"+strconv.Itoa(idEnte), &ente);err==nil{
+					beego.Info(ente)
+				if ente!=nil {
+					done := make(chan interface{})
+					defer close(done)
+					resch := optimize.GenChanInterface(ente...)
+					chentes := optimize.Digest(done, getOrganizacion, resch, nil)
 
+					for organizacion := range chentes {
+						beego.Info(organizacion)
+						//respuesta = append(respuesta, organizacion.(map[string]interface{}))
+					}
+					c.Data["json"] = respuesta
+				}
+
+			}else{
+  			beego.Error(err);
+			}
+
+}else{
+	beego.Error(err);
+}
+c.ServeJSON()
+}
+
+func getTipoOrganizacion(tipoEnte int) (f func(data map[string]interface{}, params ...interface{}) interface{}){
+	switch tipoEnte {
+		case 3:
+			return getOrganizacionEnte
+		default:
+			return nil
+	}
+}
+
+func getOrganizacionEnte(ente map[string]interface{}, params ...interface{}) interface{} {
+	var idEnte int64
+	var organizacion interface{}
+	idEnte = int64(ente["Id"].(float64))
+	beego.Error(beego.AppConfig.String("coreOrganizacionService")+"organizacion?limit=-1&query=Ente:"+strconv.FormatInt(idEnte,10))
+	if err := request.GetJson(beego.AppConfig.String("coreOrganizacionService")+"organizacion?limit=-1&query=Ente:"+strconv.FormatInt(idEnte,10), &organizacion); err == nil {
+	if organizacion != nil {
+		return organizacion
+	}else{
+		return map[string]interface{}{"Code": "E_0458", "Body": "Core New Service", "Type": "error"}
+	}
+	}else{
+		return map[string]interface{}{"Code": "E_0458", "Body": "Core Organizacion Service", "Type": "error"}
+	}
+}
+
+func getOrganizacion(ente interface{}, params ...interface{}) (res interface{}) {
+ enteMap := ente.(map[string]interface{})
+	if funcion := getTipoOrganizacion(int(enteMap["TipoEnte"].(map[string]interface{})["Id"].(float64))); funcion != nil {
+			res = funcion(enteMap, params)
+		} else {
+			beego.Error("Error")
+			return enteMap
+		}
+
+	beego.Error("respuesta",res)
+	return
 }
 
 // Put ...
