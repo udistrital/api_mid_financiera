856942f044cc0c677e591af0e2164aad0c0e8385
miguelramirez93 <darckzios@gmail.com>
2018-08-17 15:37:56 -0500

    Se agrega funcion de migracion para cdp a mongo

--- controllers/migracion_mongo.go
+++ controllers/migracion_mongo.go
@@ -3,9 +3,12 @@ package controllers
 import (
 	"errors"
 	"strconv"
+	"time"
 
 	"github.com/astaxie/beego"
 	"github.com/manucorporat/try"
+	"github.com/udistrital/api_mid_financiera/models"
+	"github.com/udistrital/utils_oas/formatdata"
 	"github.com/udistrital/utils_oas/request"
 )
 
@@ -81,6 +84,111 @@ func migrarApropiacion(vigencia int) (res interface{}, err error) {
 			} else {
 				serviceResponse = append(serviceResponse, "Error Migrate "+strconv.Itoa(int(data["Id"].(float64))))
 			}
+		}
+		res = serviceResponse
+	}).Catch(func(e try.E) {
+		beego.Error("error ", e)
+		err = errors.New(e.(string))
+	})
+
+	return
+}
+
+// MigrarMovimiento ...
+// @Title MigrarMovimiento
+// @Description Get Arbol Rubros By UE
+// @Param	vigencia		path 	int64	true		"vigencia a migrar"
+// @Success 200 {object} models.Rubro
+// @Failure 403
+// @router /MigrarMovimiento/:tipo/:vigencia [get]
+func (c *MigracionMongoController) MigrarMovimiento() {
+	response := make(map[string]interface{})
+	var f func(int) (interface{}, error)
+	try.This(func() {
+		vigencia, err := c.GetInt(":vigencia")
+		if err != nil {
+			panic("E_0458")
+		}
+		tipo := c.GetString(":tipo")
+		switch tipo {
+		case "Cdp":
+			f = migrarCdp
+		case "Rp":
+			//f = migrarRp
+		default:
+			panic("Migration Error")
+		}
+		if data, err := f(vigencia); err == nil {
+			c.Data["json"] = data
+		} else {
+			panic("Migration Error")
+		}
+
+	}).Catch(func(e try.E) {
+		response["Type"] = "Error"
+		response["Code"] = e
+		c.Data["json"] = response
+	})
+
+	c.ServeJSON()
+}
+
+func migrarCdp(vigencia int) (res interface{}, err error) {
+	try.This(func() {
+		var (
+			cdpData          []map[string]interface{}
+			mongoData        map[string]interface{}
+			serviceResponse  []interface{}
+			externalResponse map[string]interface{}
+			afectacionArray  []interface{}
+			afetcacionData   map[string]interface{}
+		)
+
+		// uri := urlCrud + "/disponibilidad?limit=-1&query=Vigencia:" + strconv.Itoa(vigencia)
+		uri := urlCrud + "/disponibilidad?limit=-1&query=Vigencia:" + strconv.Itoa(vigencia)
+		if err := request.GetJson(uri, &cdpData); err != nil {
+			beego.Info(err.Error())
+			panic("Crud Service Error")
+		}
+		uri = urlMongo + "/arbol_rubro_apropiaciones/RegistrarMovimiento/Cdp"
+		for _, data := range cdpData {
+			mongoData = make(map[string]interface{})
+			mongoData = data
+			dateStr := mongoData["FechaRegistro"].(string)
+			mongoData["Vigencia"] = strconv.Itoa(int(mongoData["Vigencia"].(float64)))
+			t, err := time.Parse(time.RFC3339, dateStr)
+			if err != nil {
+				panic(err.Error())
+			}
+			dataAfectacion := mongoData["DisponibilidadApropiacion"].([]interface{})
+			for _, data := range dataAfectacion {
+				afetcacionData = make(map[string]interface{})
+				// dispAfectInt := make(map[string]interface{})
+				dispAfect := models.DisponibilidadApropiacion{}
+				err = formatdata.FillStruct(data, &dispAfect)
+				if err != nil {
+					panic(err.Error())
+				}
+				afetcacionData["Apropiacion"] = dispAfect.Apropiacion.Id
+				afetcacionData["FuenteCodigo"] = dispAfect.FuenteFinanciamiento.Codigo
+				afetcacionData["FuenteNombre"] = dispAfect.FuenteFinanciamiento.Nombre
+				afetcacionData["Rubro"] = dispAfect.Apropiacion.Rubro.Codigo
+				afetcacionData["UnidadEjecutora"] = strconv.Itoa(int(dispAfect.Apropiacion.Rubro.UnidadEjecutora))
+				afetcacionData["Valor"] = dispAfect.Valor
+				afectacionArray = append(afectacionArray, afetcacionData)
+			}
+			mongoData["Afectacion"] = afectacionArray
+			mongoData["MesRegistro"] = strconv.Itoa(int(t.Month()))
+			if err = request.SendJson(uri, "POST", &externalResponse, &mongoData); err == nil {
+				if externalResponse["Type"].(string) == "success" {
+					//serviceResponse = append(serviceResponse, "Success Migrate "+strconv.Itoa(int(data["Id"].(float64))))
+				} else {
+					serviceResponse = append(serviceResponse, "Error Migrate "+strconv.Itoa(int(data["Id"].(float64))))
+				}
+			} else {
+				serviceResponse = append(serviceResponse, "Error Migrate "+strconv.Itoa(int(data["Id"].(float64))))
+			}
+			afectacionArray = nil
 		}
 		res = serviceResponse
 	}).Catch(func(e try.E) {
