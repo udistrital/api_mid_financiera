ba3e593785bbe888ac693209d30ca347e1cafec2
davidmm07 <moralesl07@hotmail.com>
2018-10-23 11:25:36 -0500

    se agrega ruta proveedores y registrodegiroDescuentos

--- controllers/giro.go
+++ controllers/giro.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"encoding/json"
+	"strconv"
 	"strings"
 
 	"github.com/astaxie/beego"
@@ -94,20 +95,24 @@ func (c *GiroController) Delete() {
 func (c *GiroController) CreateGiro() {
 	defer c.ServeJSON()
 	var giro map[string]interface{}
-	// var resProveedor []map[string]interface{}
-	// var idCuentasEspeciales []int
+	var resProveedor map[string]interface{}
+	var idCuentasEspeciales map[string]interface{}
 	var response map[string]interface{}
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &giro); err == nil {
 		//beego.Error("valor giro ", giro)
-		if err = request.SendJson("http://"+beego.AppConfig.String("Urlcrud")+":"+beego.AppConfig.String("Portcrud")+"/"+beego.AppConfig.String("Nscrud")+"/giro/RegistrarGiro", "POST", &response, giro); err == nil {
+		urlcrud := "http://" + beego.AppConfig.String("Urlcrud") + ":" + beego.AppConfig.String("Portcrud") + "/" + beego.AppConfig.String("Nscrud")
+		if err = request.SendJson(urlcrud+"/giro/RegistrarGiro", "POST", &response, giro); err == nil {
 			if strings.Compare(response["Type"].(string), "success") == 0 {
-			// for _, element := range response["OrdenesPago"].(map[string]interface{}) {
-			// 	err = request.GetJson("http://"+beego.AppConfig.String("Urlcrud")+":"+beego.AppConfig.String("Portcrud")+"/"+beego.AppConfig.String("Nscrud")+"/giro/GetCuentasEspeciales", &idCuentasEspeciales)	
-								
-			// 	if err = request.GetJson("http://"+beego.AppConfig.String("AdministrativaAmazonService")+"informacion_proveedor/?query=NumDocumento:"+numberIdStr+"&limit=1", &resProveedor); err== nil {
-					
-			// 	}
-			// }
+				for _, element := range response["OrdenesPago"].([]interface{}) {
+					err = request.GetJson(urlcrud+"/giro/GetCuentasEspeciales?idordenpago="+strconv.FormatFloat(element.(map[string]interface{})["Id"].(float64), 'f', -1, 64), &idCuentasEspeciales)
+					for _, cuenta := range idCuentasEspeciales {
+						if err = request.GetJson("http://"+beego.AppConfig.String("AdministrativaAmazonService")+"informacion_proveedor/?query=Id:"+cuenta.(map[string]interface{})["informacion_persona_juridica"].(string)+"&limit=1", &resProveedor); err == nil {
+							if err = request.SendJson(urlcrud+"/giro/RegistrarGiroDescuentos?idcuenta="+cuenta.(map[string]interface{})["cuenta_especial"].(string)+"&idordenpago="+element.(map[string]interface{})["Id"].(string)+"&idgiro="+response["idNewGiro"].(string), "POST", &response, &resProveedor); err == nil {
+
+							}
+						}
+					}
+				}
 				c.Data["json"] = models.Alert{Type: "success", Code: "S_543", Body: response["Body"]}
 				c.Ctx.Output.SetStatus(201)
 			} else {
